bcb8ace1c1e1141e23ae3665f6381d35978eb291
cornernote <brett@mrphp.com.au>
2019-12-23 12:18:21 +1030

    initial commit

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="refresh" content="5" />
+    <title>System Status</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700,300|Droid+Sans+Mono' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+
+</body>
+</html>
